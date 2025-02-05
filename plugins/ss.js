a169884df0d646c840bd9770b2c3b957e20ccd65
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-02-05 20:45:24 +0530

    Update ss.js

--- plugins/ss.js
+++ plugins/ss.js
@@ -1,4 +1,4 @@
-const { izumi, mode, getUrl } = require("../lib/");
+const { izumi, mode, getUrl, formatTime } = require("../lib/");
 const config = require("../config");
 
 izumi(
@@ -13,13 +13,51 @@ izumi(
             match = match || message.reply_message.text;
             if (!match) return await message.reply(`*_Need a link_*\n*eg:- .fullss https://github.com/sataniceypz/Izumi-v3*`);
             let url = await getUrl(match);
-            let res = apiUrl + `api/screenshot?url=${url}&type=phone&full=true&apikey=maskser`;
+            let res = `https://perferct-circle.serv00.net/ss.php?q=${url}`;
             await message.sendFromUrl(res);
         } catch (error) {
             await message.reply('Failed to capture screenshot.');
         }
     }
 );
+
+izumi(
+    {
+        pattern: "wm",
+        fromMe: mode,
+        desc: "wame generator",
+        type: "downloader",
+    },
+    async (message, match, m) => {
+        try {
+            match = match || message.reply_message.text;
+            if (!message.quoted) return message.reply("_*Reply to a user*_");
+            let sender = 'https://wa.me/' + (message.reply_message.sender || message.mention[0] || message.text).split('@')[0];
+            await message.reply(sender);
+        } catch (error) {
+            await message.reply('Failed to catch You.');
+        }
+    }
+);
+
+izumi({
+	pattern: 'alive',
+	fromMe: mode,
+	desc: 'Get bots runtime',
+	type: 'info'
+}, async (message, match, client) => {
+    let sender = 'Iam Working Well 🤖 ' + (formatTime(process.uptime()));
+	await message.reply(sender);
+});
+
+izumi({
+	pattern: 'readmore ?(.*)',
+        fromMe: mode,
+        desc: readmore generator',
+        type: 'info'
+}, async (message, match, client) => {
+    await message.reply(match.replace(/\+/g, (String.fromCharCode(8206)).repeat(4001)))
+});
 
 izumi(
     {
